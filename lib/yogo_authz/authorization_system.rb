750e657f55271fa0c8be4a7c074320e189d5147c
Robbie Lamb <robbie.lamb@gmail.com>
2009-08-20 15:24:31 -0600

    Reworked the authorization system. More sane now.

--- lib/yogo_authz/authorization_system.rb
+++ lib/yogo_authz/authorization_system.rb
@@ -9,51 +9,37 @@ module YogoAuthz
     def self.included(base)
       base.send :include, AuthorizationSystemInstanceMethods
       base.send :extend, AuthorizationSystemClassMethods
-      
-      base.send :class_inheritable_array, :authorization_requirements
+
+      base.send :class_inheritable_accessor, :authorization_requirements
       base.send :authorization_requirements=, []
-    
-      base.send :hide_action, :authorization_requirements, :authorization_requirements=
-    
-      # base.send :wants_logged_in
-    
+      base.send :class_inheritable_accessor, :declaired_authz
+      base.send :declaired_authz=, false
+
+      base.send :hide_action, 
+                :authorization_requirements,
+                :authorization_requirements=,
+                :declaired_authz,
+                :declaired_authz=
+
+      base.send :before_filter, :check_authorization
     end
 
     module AuthorizationSystemClassMethods
-    
-      # ouch ouch ouch 
-      # This replaces the method defined in extlib, because we want the one
-      # defined in inheritable_attributes.rb
-      def class_inheritable_reader(*syms) #nodoc
-        syms.each do |sym|
-          next if sym.is_a?(Hash)
-          class_eval <<-EOS
-            def self.#{sym}                        # def self.before_add_for_comments
-              read_inheritable_attribute(:#{sym})  #   read_inheritable_attribute(:before_add_for_comments)
-            end                                    # end
-                                                   #
-            def #{sym}                             # def before_add_for_comments
-              self.class.#{sym}                    #   self.class.before_add_for_comments
-            end                                    # end
-          EOS
-        end
-      end
     
       # This is where initialzation could be, should be happening.
       # Baised on options passed in, decide how to do it
       #
       # Example Usage
       #
-      #   authorize_default
-      #   authorize_group
       
-      def require_authorization(type, options = {})
-        options.assert_valid_keys(:if, :unless, :only, :for, :only_if_logged_in?, :except, :redirect_url, :render_url, :status)
+      def require_authorization(type, values, options = {})
+        options.assert_valid_keys(:if, :unless, :only, :for, :except, :redirect_url, :render_url, :status)
       
-        unless @before_filter_declaired ||= false
-          @before_filter_declared = true
-          before_filter :check_authorization
+        unless self.declaired_authz ||= false
+          self.declaired_authz = true
         end
+      
+        values = [values] unless Array === values
       
         for key in [:only, :except]
           if options.has_key?(key)
@@ -63,73 +49,70 @@ module YogoAuthz
         end
       
         self.authorization_requirements ||=[]
-        self.authorization_requirements << {:type => type, :options  => options}
+        self.authorization_requirements << {:type => type, :values => values, :options  => options}
       end
     
     def method_missing(method_id, *arguments)
-      method_type = method_id.to_s.match(/^(forbid|wants)_([_a-zA-Z]\w*)$/)
-      super if method_type.nil?
+      method_type = method_id.to_s.match(/^(authorize)_([_a-zA-Z]\w*)$/)
+      super if method_type.nil? && !respond_to?("user_is_in_#{method_type[2]}?")
       require_authorization(method_type[2], *arguments)
     end
     
-    def forbid_handler(*arguements)
-      logger.debug { "You shouldn't use forbid methods.\nDeny first, allow later." }
-      # But isn't this fun.
-    end
-    
     # Returns the action that should be taken
-    def next_authorized_action_for(web_user, params = {}, binding = self.binding)
-      return nil unless Array===self.auth_requirements
+    # TODO: Simpilfy this
+    def next_authorized_action_for(user, params = {}, binding = self.binding)
+      return nil unless Array===self.authorization_requirements
       self.authorization_requirements.each do |requirement|
         type = requirement[:type]
+        values = requirement[:values]
         options = requirement[:options]
-        
+      
         if options.has_key?(:only)
           next unless options[:only].include?( (params[:action]||"index").to_sym )
         end
-        
+      
         if options.has_key?(:for)
           next unless options[:for].include?( (params[:action]||"index").to_sym )
         end
-        
+      
         if options.has_key?(:except)
           next if options[:except].include?( (params[:action]||"index").to_sym )
         end
-        
+      
         if options.has_key?(:if)
           next unless ( String==options[:if] ? eval(options[:if], binding) : options[:if].call(params) )
         end
-        
+      
         if options.has_key?(:unless)
           next if ( String===options[:unless] ? eval(options[:unless], binding) : options[:unless].call(params) )
         end
-        
-        puts type
-        
-        # values.each{
-                 return { :url => options[:redirect_url], :status => options[:status] } unless 
-                 (web_user and controller_permissions(web_user, params))
-               # } unless (web_user == :false || web_user == false)
-               
+      
+        puts "The Type is #{type}"
+        values.each { |value| 
+          return { :url => options[:redirect_url], :status => options[:status] } unless 
+                   (user and send("user_is_in_#{type}?", user, value))
+        } unless (user == :false || user == false)
+             
       end
       return nil
     end
     
     def url_authorized?(web_user, params = {}, binding = self.binding)
-      unless self.next_authorized_action_for(web_user, params, binding)
-        return false
+      # debugger
+      if !web_user.nil? 
+        return self.next_authorized_action_for(web_user, params, binding).nil? if self.has_inline_permissions?
+        return !self.controller_permissions(web_user, params).empty? if self.has_db_permissions?(params[:controller], params[:action])
       end
       return true
     end
     
-    # The heart of the system.
      # Returns permissions the given user has for the current controller and action
      #
      def controller_permissions(web_user = nil, params = {})
        controller_name = params[:controller]
        action_name     = params[:action]
      
-       groups = current_web_user.groups
+       groups = web_user.groups
      
        permissions = groups.collect{ |g|
          g.self_and_ancestors.
@@ -143,7 +126,19 @@ module YogoAuthz
     def reset_authorization_requirements!
       self.authorization_requirements.clear
     end
+
+    def has_db_permissions?(controller, action)
+      !YogoAuthz::Permission.all(:controller_name => controller,
+                                 :action_name     => action).empty?
+    end
     
+    def has_inline_permissions?
+      !self.authorization_requirements.empty?
+    end
+
+    def user_is_in_group?(user, group)
+      user.has_group?(group)
+    end
 
     
     end # module AuthorizationSystemClassMethods
@@ -154,9 +149,11 @@ module YogoAuthz
     
       # What to do when the user is denied?
       def authorization_denied
+        puts "authorization denied"
         store_location
         flash[:notice] = "You've been denied."
         redirect_to new_user_session_url
+        #TODO: Redirect back or default
         return false
       end
     
@@ -165,15 +162,13 @@ module YogoAuthz
         true
       end
     
+      # Checks the authorization of the current user.
+      # Returns the proper results if
       def check_authorization
         # return authorization_denied if current_web_user.nil?
         # return authorized if admin_groups.length > 0
         return authorization_denied unless self.url_authorized?(params)
         return authorized
-      end
-    
-      def admin_groups
-        current_web_user.groups.all(:sysadmin => true)
       end
       
       def url_authorized?(params = {})
