9cdadd494b6d6aeb6903b6ee59ca3ab505adaa17
Robbie Lamb <robbie.lamb@gmail.com>
2009-12-25 07:58:28 -0700

    Attempted to make method_missing helpful

--- app/models/yogo/anonymous_user.rb
+++ app/models/yogo/anonymous_user.rb
@@ -8,4 +8,13 @@ class Yogo::AnonymousUser
   def has_group?(value)
     return value.eql?(Yogo::Settings[:anonymous_user_group])
   end
+  
+  def method_missing(method, *args)
+    if eval(Yogo::Settings[:user_class]).new.respond_to?(method)
+      raise Yogo::UserMethodOnAnonymousUser.new("undefined method '#{method}' for #{self}. Did you mean to call it on a #{Yogo::Settings[:user_class]} class instead?")
+    else
+      raise NoMethodError.new("undefined method '#{method}' for #{self}")
+    end
+    
+  end
 end
